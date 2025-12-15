942059fbb78828c6a5a6dc92abd7108b4b44695f
Alcino-Luvualo <Kenikenilson@gmail.com>
2025-12-15 15:10:07 +0100

    Simplify user registration code by destructuring request body

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,7 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  const  {username, password} = req.body
 
   if (username && password){
     if (isValid(username)) {
